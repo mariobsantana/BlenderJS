082c5c346bed21e5e8698bf0d8fe9c3d87294da3
Mario B. Santana <donmaoccio@icloud.com>
2022-03-11 16:17:51 -0600

    all done

--- js/engine.js
+++ js/engine.js
@@ -1,11 +1,30 @@
 let blenderStatus = "off";
+let blender = document.getElementById("blender");
+let blenderSound = document.getElementById("blenderSound");
+let buttonSound = document.getElementById("buttonSound");
+
 
 const blenderButton =() => {
     if (blenderStatus == "off") {
         blenderStatus = "on";
-        console.log("Blender is on");
+        blender.classList.add("blender-On");
+        brrrbrbrrr();
+        /* console.log("Blender is on"); */
     } else {
         blenderStatus = "off";
-        console.log("Blender is off");
+        blender.classList.remove("blender-On");
+        brrrbrbrrr();
+        /* console.log("Blender is off"); */
+    }
+}
+
+const brrrbrbrrr = () => {
+    if (blenderSound.paused) {
+        buttonSound.play();
+        blenderSound.play();
+    } else {
+        buttonSound.play();
+        blenderSound.pause();
+        blenderSound.currentTime = 0;
     }
 }
